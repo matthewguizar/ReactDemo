03dae0df0c5adf345cd9499ece796064254c3c45
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-22 16:59:23 -0800

    passing data to react components dynamically

--- src/App.js
+++ src/App.js
@@ -27,12 +27,21 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              <TodoRowItem/>
-              <tr>
-                <th scope='row'>2</th>
-                <td>get haircut</td>
-                <td>Eric</td>
-              </tr>
+              <TodoRowItem 
+              rowNumber={todos[0].rowNumber}
+              rowDescription={todos[0].rowDescription}
+              rowAssigned={todos[0].rowAssigned}
+              />
+              <TodoRowItem 
+              rowNumber={todos[1].rowNumber}
+              rowDescription={todos[1].rowDescription}
+              rowAssigned={todos[1].rowAssigned}
+              />
+              <TodoRowItem 
+              rowNumber={todos[2].rowNumber}
+              rowDescription={todos[2].rowDescription}
+              rowAssigned={todos[2].rowAssigned}
+              />
             </tbody>
           </table>
         </div>
