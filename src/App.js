c1114ca3bf839c5805c1ceeab89a7b8b414d232d
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-22 16:45:32 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoRowItem from './components/TodoRowItem';
 
 function App() {
+
+  const todos = [
+    {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
+    {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
+    {rowNumber: 3, rowDescription: 'Make Dinner', rowAssigned: 'User One'}
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //margin-top 5
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
+          Your Todo's
+        </div>
+        <div className='card-body'>
+          <table className='table table-hover'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TodoRowItem/>
+              <tr>
+                <th scope='row'>2</th>
+                <td>get haircut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+      </div>
     </div>
   );
 }
