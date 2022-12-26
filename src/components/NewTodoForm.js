dafcb62999e8a97ae24024a7e9fa135ee4132522
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-26 14:12:17 -0800

    adding user input for form

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
 function NewTodoForm() {
+    const [description, setDescription] = useState('');
+    const [assigned, setAssigned] = useState('');
+
+    const descriptionChange = (event) => {
+        console.log('description', event.target.value);
+        setDescription(event.target.value);
+    }
+
+    const assignedChanged = (event) => {
+        console.log("assigned", event.target.value)
+        setAssigned(event.target.value);
+
+    }
+
     return (
+       
+
         <div className="mt-5">
             <form>
                 {/* margin-bottom */}
                 <div className="mb-3">
                     <label className="form-label">Assigned</label>
-                    <input type="text" className="form-control" required></input>
+                    <input 
+                    type="text" 
+                    className="form-control" 
+                    required
+                    onChange={assignedChanged}
+                    value={assigned}
+                    ></input>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Description</label>
-                    <textarea className="form-control" rows={3} required></textarea>
+                    <textarea 
+                    className="form-control" 
+                    rows={3} 
+                    required
+                    onChange={descriptionChange}
+                    value={description}
+                    ></textarea>
                 </div>
                 <button type="button" className="btn btn-primary mt-3">Add Todo</button>
             </form>
